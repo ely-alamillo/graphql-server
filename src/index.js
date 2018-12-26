656fff8eb4864eb50a9129decc2b2b42efe549fe
Ely Alamillo <eleazar.alamillo@valic.com>
2018-12-26 15:16:59 -0600

    exploring prisma functionality

--- src/index.js
+++ src/index.js
@@ -1,22 +1,54 @@
 const { GraphQLServer } = require('graphql-yoga');
-
-const typeDefs = `
-type Query {
-  description: String
-}
-`;
+const { Prisma } = require('prisma-binding');
 
 const resolvers = {
   Query: {
-    description: () => `test query`
+    posts(parent, args, ctx, info) {
+      return ctx.db.query.posts({}, info);
+    },
+    post(parent, args, ctx, info) {
+      return ctx.db.query.post({ where: { id: args.id } }, info);
+    }
+  },
+  Mutation: {
+    createDraft(parent, { title, content }, ctx, info) {
+      return ctx.db.mutation.createPost(
+        {
+          data: {
+            title,
+            content
+          }
+        },
+        info
+      );
+    },
+    deletePost(parent, { id }, ctx, info) {
+      return ctx.db.mutation.deletePost({ where: { id } }, info);
+    },
+    publish(parent, { id }, ctx, info) {
+      return ctx.db.mutation.updatePost(
+        {
+          where: { id },
+          data: { published: true }
+        },
+        info
+      );
+    }
   }
 };
 
 const server = new GraphQLServer({
-  typeDefs,
-  resolvers
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql', // the generated Prisma DB schema
+      endpoint: 'https://eu1.prisma.sh/ely-alamillo/database/dev', // the endpoint of the Prisma DB service
+      secret: 'mysecret123', // specified in database/prisma.yml
+      debug: true // log all GraphQL queries & mutations
+    })
+  })
 });
 
-server.start(() =>
-  console.log(`The server is running on http://localhost:4000`)
-);
+server.start(() => console.log('Server is running on http://localhost:4000'));
